daa48a235b259b3204ca9df24b3b3fd9c59d062b
Heon Soo Park <hspark220@gmail.com>
2023-03-07 08:52:04 -0500

    Finish mergeSort

--- script.js
+++ script.js
@@ -24,42 +24,27 @@ const fibsRec = num => {
 const mergeSort = array => {
     const middle = (array.length/2)%2 == 0 ? array.length/2 : array.length/2+1
     if (array.length <= 2) {
-        let copy = [];
-        if (array.length == 1) {
-            copy.push(array[0]);
-            return copy;
-        } else {
-            if (array[0] > array[1]) {
-                copy.push(array[1]);
-                copy.push(array[0]);
-                return copy;
-            } else {
-                copy.push(array[0])
-                copy.push(array[1]);
-                return copy;
-            }
-        }
-    } else {
-        let array1 = mergeSort(array.splice(0,middle));
-        let array2 = mergeSort(array.splice(-middle));
-        const result = [];
-
-        while (array1.length > 0 && array2.length > 0) {
-            if (array1[0] > array2[0]) {
-                result.push(array2.shift());
-            } else {
-                result.push(array1.shift());
-            }
-        }
-        if (array1.length == 0) {
-            result.concat(array2);
-        } else {
-            result.concat(array1);
-        }
-        return result;
+        return array.length==2 ? sort([array[0]],[array[1]]) : array;
+    } else  {
+        return sort(mergeSort(array.splice(0,middle)), mergeSort(array.splice(-middle)));
     }
+}
+
+const sort = (array1, array2) => {
+    let result = [];
+
+    while (array1.length > 0 && array2.length > 0) {
+        if (array1[0] > array2[0]) {
+            result.push(array2.shift());
+        } else {
+            result.push(array1.shift());
+        }   
+    }
+
+    result = array1.length==0 ? result.concat(array2) : result.concat(array1);
+    return result;
 }
 
 // console.log(fibs(10));
 // console.log(fibsRec(10));
-console.log(mergeSort([3,2,1,3,5,7,8]));
+ console.log(mergeSort([2,1,4,5,10,32,11,1,3,8,3,200,4,5,600]));
